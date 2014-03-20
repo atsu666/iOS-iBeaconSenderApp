9207cd2d9950cd61e3c0de27690b9806a89c5d97
atsu666 <ito@appleple.com>
2014-03-20 15:37:25 +0900

    Initial Commit

---
+++ iBeaconSenderApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBeaconSenderApp
+//
+//  Created by Atsushi Ito on 2014/03/20.
+//  Copyright (c) 2014年 atsu666. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iBeaconSenderApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeaconSenderApp
+//
+//  Created by Atsushi Ito on 2014/03/20.
+//  Copyright (c) 2014年 atsu666. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
